f5bd1affc014c70e45011f6853df7e62541e1041
taiga.yajima <mazdaspeed_reamemiya@yahoo.co.jp>
2020-04-07 12:09:33 +0000

    kadai complete2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,10 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-      @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -53,4 +56,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content,:status,)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
