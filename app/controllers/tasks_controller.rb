0a332e10c9a186f601f23447a7f5c8a3a3b80cbd
taiga.yajima <mazdaspeed_reamemiya@yahoo.co.jp>
2020-04-08 01:50:45 +0000

    kadai complete3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user , only: [:show, :edit, :update, :destroy]
   def index
     if logged_in?
       @task = current_user.tasks.build  # form_with 用
@@ -48,10 +47,6 @@ class TasksController < ApplicationController
   end
 
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content,:status,)
